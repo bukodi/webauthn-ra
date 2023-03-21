ca830190aa94838de4dec4ed6e857459a5388369
László Bukodi <bukodi@gmail.com>
2023-03-21 09:11:54 +0100

    Improve auditable/Persister

--- pkg/util/auditable/persister.go
+++ pkg/util/auditable/persister.go
@@ -6,8 +6,8 @@ var ErrMasherHashChanged = fmt.Errorf("master hash changed")
 
 type TxToSave interface {
 	Id() [32]byte
-	PrevId() [32]byte
-	Entries() []EntryChange
+	PrevTxId() [32]byte
+	Changes() []EntryChange
 	Signature() []byte
 }
 
@@ -15,7 +15,13 @@ type EntryChange interface {
 	// The entry to save, if nil, the entry will be deleted
 	Entry() SetEntry
 	// PrevId the id of the previous version of the entry. If NilId, this is a new entry
-	PrevId() [32]byte
+	PrevId() Id
+
+	// NextId the id of the next version of the entry. If NilId, this the actual version of the entry
+	NextId() Id
+
+	// The actual id of the entry. It is always equals to SHA256( PrevID || Entry as bytes )
+	Id() Id
 }
 
 type Persister interface {
